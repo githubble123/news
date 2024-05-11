587f48a5454b72a0a59bc94eb751766b402e1b61
yongick Kim <yongikgo@gmail.com>
2024-05-11 09:51:11 +0900

    fifth commit

--- main.js
+++ main.js
@@ -9,6 +9,54 @@ var url = new URL(
   `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${apiKey}`
 );
 
+var weatherIcon = {
+  "01": "fas fa-sun",
+  "02": "fas fa-cloud-sun",
+  "03": "fas fa-cloud",
+  "04": "fas fa-cloud-meatball",
+  "09": "fas fa-cloud-sun-rain",
+  10: "fas fa-cloud-showers-heavy",
+  11: "fas fa-poo-storm",
+  13: "far fa-snowflake",
+  50: "fas fa-smog",
+};
+
+var apiURI =
+  "http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=c1822a962ec35674f8e9c91e4ca74f1c";
+$.ajax({
+  url: apiURI,
+  dataType: "json",
+  type: "GET",
+  async: "false",
+  success: function (resp) {
+    var icon = resp.weather[0].icon.substr(0, 2);
+    var weatherDescription = resp.weather[0].main;
+    var temp = Math.floor(resp.main.temp - 273.15) + "°C";
+    var humidity = "습도 : " + resp.main.humidity + " %";
+    var wind = "바람 : " + resp.wind.speed + " m/s";
+    var city = "서울";
+    var cloud = "구름 : " + resp.clouds.all + " %";
+    var tempMin =
+      "최저 온도 : " + Math.floor(resp.main.temp_min - 273.15) + "°C";
+    var tempMax =
+      "최고 온도 : " + Math.floor(resp.main.temp_max - 273.15) + "°C";
+
+    $("#weatherIcon").append(
+      '<i class="' +
+        weatherIcon[icon] +
+        ' fa-5x" style="height : 50px; width : 50px;"></i>'
+    );
+    $("#weatherDescription").prepend(weatherDescription);
+    $("#currentTemp").prepend(temp);
+    $("#humidity").prepend(humidity);
+    $("#wind").prepend(wind);
+    $("#city").append(city);
+    $("#cloud").append(cloud);
+    $("#tempMin").append(tempMin);
+    $("#tempMax").append(tempMax);
+  },
+});
+
 var totalResult = 0;
 var page = 1;
 const pageSize = 10;
@@ -35,7 +83,7 @@ async function getNews() {
   } catch (error) {
     errorRender(error.message);
   }
-};
+}
 
 async function getLatestNews() {
   url = new URL(
@@ -46,7 +94,7 @@ async function getLatestNews() {
   var data = await response.json();
   getNews();
   console.log("newsList", newsList);
-};
+}
 
 async function getNewsByCategory(event) {
   var category = event.target.id;
@@ -54,11 +102,10 @@ async function getNewsByCategory(event) {
     `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${apiKey}`
   );
   getNews();
-};
+}
 
 function render() {
   var newsHTML = ``;
-
   newsHTML = newsList
     .map(
       (news) => ` <div class="news row">
@@ -66,9 +113,7 @@ function render() {
             <img class="newsImageSize"src="${news.urlToImage}">
         </div>
         <div class="col-lg-8">
-            <a class="title" target="_blank" href="${news.url}">${
-        news.title
-      }</a>
+            <a class="title" target="_blank" href="${news.url}">${news.title}</a>
             <p> ${news.description}</p>
             <div>
                 ${news.source.name} * ${news.publishedAt}
@@ -76,10 +121,10 @@ function render() {
         </div>
     </div>`
     )
-    .join("");
+    .join(""); //,없애기
 
   document.getElementById("newsBoard").innerHTML = newsHTML;
-};
+}
 
 function errorRender(errorMessage) {
   var errorHTML = `
@@ -87,7 +132,7 @@ function errorRender(errorMessage) {
     ${errorMessage}
     </div>`;
   document.getElementById("newsBoard").innerHTML = errorHTML;
-};
+}
 
 function paginationRender() {
   var pageGroup = Math.ceil(page / groupSize);
@@ -102,9 +147,6 @@ function paginationRender() {
   }
 
   var paginationHTML = `<li class="page-item" onclick="moveToPage(${page - 1})">
-        <a class="page-link" aria-label="Previous">
-          <span aria-hidden="true">&laquo;</span>
-        </a>
       </li>`;
 
   for (var i = firstPage; i <= lastPage; i++) {
@@ -118,13 +160,10 @@ function paginationRender() {
   }
 
   paginationHTML += `<li class="page-item" onclick="moveToPage(${page - 1})">
-    <a class="page-link" aria-label="Next">
-        <span aria-hidden="true">&raquo;</span>
-    </a>
     </li>`;
 
   document.querySelector(".pagination").innerHTML = paginationHTML;
-};
+}
 
 getLatestNews();
 
@@ -134,9 +173,9 @@ async function getNewsByKeyword() {
     `https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${apiKey}`
   );
   getNews();
-};
+}
 
 function moveToPage(pageNum) {
   page = pageNum;
   getNews();
-};
+}
